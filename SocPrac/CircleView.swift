fa1ce6c06489768e60fc027d6d37d634565cc715
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-07-25 16:25:03 -0700

    first commit

---
+++ SocPrac/CircleView.swift
@@ -0,0 +1,17 @@
+//
+//  CircleView.swift
+//  SocPrac
+//
+//  Created by Allen Czerwinski on 7/25/17.
+//  Copyright © 2017 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = self.frame.width / 2
+        clipsToBounds = true
+    }
+}
